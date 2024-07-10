f03709d472baeabc3687e6c5aba0852ed9d1e3ed
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-11 00:50:16 +0530

    Commit 12 :- FEAT: Create adminRoute middleware based on user admin role

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import JWT from 'jsonwebtoken';
-
+import User from '../models/userModel.js';
 
 // Create Protected Routes Based on JWT Token
 export const protectedRoute = async(req, res, next) => {
@@ -21,3 +21,25 @@ export const protectedRoute = async(req, res, next) => {
 }
 
 
+// Create Admin Routes Based on User Role
+export const adminRoute = async(req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if(user.role !== 'admin'){
+            return res.status(401).send({
+                success: false,
+                message: 'Unauthorized User Access Please LogIn by Admin'
+            });
+        } else {
+            next();
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in AdminRoute Middleware!',
+            error,
+        });
+    }
+}
