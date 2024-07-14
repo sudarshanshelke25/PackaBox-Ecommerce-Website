d8dcfded78b3eb61a2cfb7bb9e887cd017eaacb6
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-14 18:24:00 +0530

    Commit 24 :- FEAT: Implement BoxLoader component as a spinner for data loading, use useLocation and useNavigation for page redirection

--- client/src/components/Spinners/BoxLoader.js
+++ client/src/components/Spinners/BoxLoader.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './loader.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const BoxLoader = () => {
+    const [count, setCount] =useState(6);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        const interval = setInterval(( ) => {
+            setCount((prevValue) => --prevValue)
+        }, 1000);
+
+        count === 0 && navigate('/login',{
+            state: location.pathname,
+        });
+        return () => clearInterval(interval);
+
+    }, [count, navigate, location]);
+
   return (
-    <div>BoxLoader</div>
+    <>
+        <div className="container">
+            <div className="cube-wrapper">
+                <div className="cube">
+                    <div className="sides">
+                    <div className="top" />
+                    <div className="right" />
+                    <div className="bottom" />
+                    <div className="left" />
+                    <div className="front" />
+                    <div className="back" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
-export default BoxLoader
+export default BoxLoader;
