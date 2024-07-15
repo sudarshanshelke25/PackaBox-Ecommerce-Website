fca2a552c69b49aa064001e812775ffdc88625cc
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-15 23:39:34 +0530

    Commit 28 :- FEAT: Implement Admin route to restrict access to authenticated admins only

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Login from './auth/Login';
 import Signup from './auth/Signup';
 import Forgot from './auth/Forgot';
 import PageNotFound from './pages/PageNotFound';
-import PrivateRoute from './components/Routes/Private';
+import PrivateRoute from './components/Routes/PrivateRoute';
 
 import Account from './pages/account/Account';
 import Profile from './pages/account/Profile';
@@ -16,6 +16,7 @@ import Payment from './pages/account/Payment';
 import Reviews from './pages/account/Reviews';
 import ContactUs from './pages/account/ContactUs';
 import Settings from './pages/account/Settings';
+import AdminRoute from './components/Routes/AdminRoute';
 
 
 function App() {
@@ -31,6 +32,19 @@ function App() {
         {/* Account Route */}
         <Route path="/account" element={<PrivateRoute />}>
           <Route path="" element={<Account />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="address" element={<Address />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="payments" element={<Payment />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="contactus" element={<ContactUs />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+
+        {/* Admin Route */}
+        <Route path="/admin" element={<AdminRoute />}>
+          <Route path="dashboard" element={<Account />} />
           <Route path="profile" element={<Profile />} />
           <Route path="orders" element={<Orders />} />
           <Route path="address" element={<Address />} />
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {forgotPasswordController, loginController, signupController } from '../controllers/authController.js'
-import { protectedRoute } from '../middlewares/authMiddleware.js';
+import { adminRoute, protectedRoute } from '../middlewares/authMiddleware.js';
 
 // Router Object
 const router = express.Router();
@@ -16,10 +16,16 @@ router.post('/login', loginController);
 // FORGOT PASSWORD || METHOD POST
 router.post('/forgot-password', forgotPasswordController);
 
-// Protected Routes auth
+// Protected Routes user auth
 router.get('/user-auth', protectedRoute, (req, res) => {
     res.status(200).send({ok:true})
 });
+
+// Protected Routes admin auth
+router.get('/admin-auth', protectedRoute, adminRoute, (req, res) => {
+    res.status(200).send({ok:true})
+});
+
 
 
 export default router;
