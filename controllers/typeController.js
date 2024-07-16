748ee4768707063fa9fbc965c60d20534a4238b4
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-17 01:42:46 +0530

    Commit 55 :- FEAT: Implement updateTypeController using express-formidable, fs, slugify and typeModel

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -79,7 +79,58 @@ export const getTypeController = async (req, res) => {
 
 // Update Type Controller
 export const updateTypeController = async (req, res) => {
-    
+    try {
+        // Get Type Data From Request Fields
+        const {name, slug, category}= req.fields;
+        // Get Type Logo Image From Request Files
+        const {logo}= req.files;
+        // Get Type ID From Request Params
+        const {id} = req.params;
+
+
+        // Validation for Type
+        if (!name) {
+            return res.status(401).send({
+                message: `Type Name is required!`,
+            });
+        }
+
+        if (!category ) {
+            return res.status(401).send({
+                message: `Type Category is required!`,
+            });
+        }
+
+        if (!logo && logo.size >10000) {
+            return res.status(401).send({
+                message: `Type Logo is required! & Should be less than 10MB!`,
+            });
+        }
+        
+        // Find By Id And Update Type
+        const type = await Type.findByIdAndUpdate(id, {...req.fields, slug:slugify(name)}, {new:true});
+        // Set Logo Path and Data Type
+        if (logo) {
+            type.logo.data = fs.readFileSync(logo.path);
+            type.logo.contentType = logo.type;
+        }
+        await type.save();
+
+        res.status(201).send({
+            success: true,
+            message: `${type.name}, Type Updated`,
+            type,
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: `Error in Update Type!`,
+            error,
+        });
+    }
 }
 
 // Delete Type Controller
