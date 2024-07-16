a26caf2eb1d563bc2333595e16081cce4aeb2a6b
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-17 01:41:33 +0530

    Commit 54 :- FEAT: Implement createTypeController using express-formidable, fs, slugify and typeModel

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -5,7 +5,66 @@ import fs from 'fs';
 
 // Create Type Controller
 export const createTypeController = async (req, res) => {
-    
+    try {
+        // Get Type Data From Request Fields
+        const {name, slug, category}= req.fields;
+        // Get Type Logo Image From Request Files
+        const {logo}= req.files;
+
+        // Validation for Type
+        if (!name) {
+            return res.status(401).send({
+                message: `Type Name is required!`,
+            });
+        }
+
+        if (!category ) {
+            return res.status(401).send({
+                message: `Type Category is required!`,
+            });
+        }
+
+        if (!logo && logo.size >10000) {
+            return res.status(401).send({
+                message: `Type Logo is required! & Should be less than 10MB!`,
+            });
+        }
+
+        // Get Existing Type
+        const existingType = await Type.findOne({name});
+        // Check Existing Type
+        if (existingType) {
+            return res.status(200).send({
+                success: true,
+                message: `${existingType.name}, Type Already Exists!`,
+                existingType,
+            });
+        }
+
+        // Create New Type
+        const type = await new Type({...req.fields, slug:slugify(name)});
+        // Set Logo Path and Data Type
+        if (logo) {
+            type.logo.data = fs.readFileSync(logo.path);
+            type.logo.contentType = logo.type;
+        }
+        await type.save();
+
+        res.status(201).send({
+            success: true,
+            message: `${type.name}, New Type Created`,
+            type,
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: `Error in Create Type!`,
+            error,
+        });
+    }
 }
 
 // Get All Type Controller
