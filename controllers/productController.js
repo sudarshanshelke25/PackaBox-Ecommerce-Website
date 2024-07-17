50341c3ec1003d9ac7f61860469aa735fe06e338
SudarshanShelke <sudarshanshelke@gmail.com>
2024-07-17 16:58:16 +0530

    Commit 62 :- FEAT: Implement updateProductController using express-formidable, fs, slugify and productModel

--- controllers/productController.js
+++ controllers/productController.js
@@ -106,7 +106,93 @@ export const getProductController = async (req, res) => {
 
 // Update Product Controller
 export const updateProductController = async (req, res) => {
+    try {
+        // Get Product Data From Request Fields
+        const {name, slug, description, price, discout, color, stock, feature, sector, category, brand, type, rate, days}= req.fields;
+        // Get Product Image From Request Files
+        const {cover}= req.files;
+        // Get Product ID From Request Params
+        const {id} = req.params;
+
+        // Validation for Brand
+        if (!name) {
+            return res.status(401).send({
+                message: `Product Name is required!`,
+            });
+        }
+
+        if (!description) {
+            return res.status(401).send({
+                message: `Product Description is required!`,
+            });
+        }
+
+        if (!price) {
+            return res.status(401).send({
+                message: `Product Price is required!`,
+            });
+        }
+
+        if (!color) {
+            return res.status(401).send({
+                message: `Product Color is required!`,
+            });
+        }
+
+        if (!sector) {
+            return res.status(401).send({
+                message: `Product Sector is required!`,
+            });
+        }
+
+        if (!category) {
+            return res.status(401).send({
+                message: `Product Category is required!`,
+            });
+        }
+
+        if (!brand) {
+            return res.status(401).send({
+                message: `Product Brand is required!`,
+            });
+        }
+
+        if (!type) {
+            return res.status(401).send({
+                message: `Product Type is required!`,
+            });
+        }
+
+        if (!cover && cover.size >10000) {
+            return res.status(401).send({
+                message: `Product Cover is required! & Should be less than 10MB!`,
+            });
+        }
     
+        // Find By Id And Update Product
+        const product = await Product.findByIdAndUpdate(id, {...req.fields, slug:slugify(name)}, {new:true});
+        // Set Cover Path and Data Type For Product
+        if (cover) {
+            product.cover.data = fs.readFileSync(cover.path);
+            product.cover.contentType = cover.type;
+        }
+        await product.save();
+
+        res.status(201).send({
+            success: true,
+            message: `${product.name}, Product Updated`,
+            product,
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: `Error in Update Product!`,
+            error,
+        });
+    }
 }
 
 // Delete Product Controller
